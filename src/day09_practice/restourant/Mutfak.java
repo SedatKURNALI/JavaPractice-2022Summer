bb849dd242693cfd6c0159c786afac30fc1c6f47
SedatKURNALI <sedatkurnali67@hotmail.com>
2022-07-25 18:04:46 +0300

    update

--- src/day09_practice/restourant/Mutfak.java
+++ src/day09_practice/restourant/Mutfak.java
@@ -2,8 +2,29 @@ package day09_practice.restourant;
 
 public class Mutfak {
 
+    public String yemekler = "adanakebeb, ufraciger, kusbasi, kusleme";
 
+    public String araSicak = "yaylacorbasi, mercimek, duguncorbasi, tarhana";
 
+    public String tatlilar = "baklava, sutlac, gullac, kazandibi, kunefe";
+
+    public String icecekler = "ayran, salgam, kola, soda";
+
+    public Mutfak() {
+    }
+
+    public Mutfak(String adanakebab, String mercimek, String baklama, String soda) {
+    }
+
+    @Override
+    public String toString() {
+        return "Mutfak{" +
+                "\nyemekler='" + yemekler + '\'' +
+                ", \naraSicak='" + araSicak + '\'' +
+                ", \ntatlilar='" + tatlilar + '\'' +
+                ", \nicecekler='" + icecekler + '\'' +
+                '}';
+    }
 
 
 
