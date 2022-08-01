2406e113238e026247fcd3f99f50aec7cfe2cd57
SedatKURNALI <sedatkurnali67@hotmail.com>
2022-08-01 17:53:48 +0300

    update

---
+++ src/day11_interviewQuestions/Q02.java
@@ -0,0 +1,17 @@
+package day11_interviewQuestions;
+
+public class Q02 {
+    public static void main(String[] args) {
+        /*
+             * Kulanicidan alinan bir Stringin ilk ve son harfini yine kulanicidan
+             *  alinan sayi kadar
+             * return eden metod yaziniz
+             *
+             * ornek:  input            output
+             *         elma  2           eaea
+             *         army  3           ayayay
+        */
+
+
+    }
+}
---
+++ src/day11_interviewQuestions/Q03.java
@@ -0,0 +1,23 @@
+package day11_interviewQuestions;
+
+import java.util.Scanner;
+
+public class Q03 {
+    public static void main(String[] args) {
+        /*
+            Ask user enter a positive number and check if it is prime or not
+            Kullanıcıdan pozitif bir sayı girmesini isteyin ve asal olup olmadığını
+            kontrol edin
+
+            Kısaca asal sayılar sadece kendine ve 1'e bölünebilen pozitif tam sayılardır.
+            1 sayısı ise asal sayı değildir. Çünkü asal sayıların sadece 2 pozitif tam
+            sayı böleni olmalıdır.
+            1 ise sadece 1'e bölünebildiği için yalnız tek böleni bulunmaktadır.
+        */
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz :");
+        int sayi = scan.nextInt();
+
+
+    }
+}
